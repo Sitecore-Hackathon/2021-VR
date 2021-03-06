2536293fcbd08cfe06bdef869baea404937c6e50
nkdram <ramkumar.nambhi@valtech.com>
2021-03-06 09:09:19 +0100

    Resolve Build Issues

---
+++ src/Foundation/ImageProcessing/code/Helpers/MediaHelper.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace VRBYOD.Foundation.ImageProcessing.Helpers
+{
+    public static class MediaHelper
+    {
+
+        public static string CalculateMd5(MediaItem media)
+        {
+            byte[] hash;
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = media.GetMediaStream())
+                {
+                    hash = md5.ComputeHash(stream);
+                }
+            }
+            return BitConverter.ToString(hash).ToLower().Replace("-", string.Empty);
+        }
+
+        public static bool RemoveFile(string Path)
+        {
+            if (!File.Exists(Path))
+                return false;
+            File.Delete(Path);
+            return true;
+        }
+    }
+}
