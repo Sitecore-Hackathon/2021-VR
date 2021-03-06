9877ccd7d20e267fae656a9f749b16f06d9839a7
nkdram <ramkumar.nambhi@valtech.com>
2021-03-06 06:44:23 +0100

    Adding a build copy of vott app

---
+++ voTT-app/build/precache-manifest.beb4d789b99f241dca744f6cdb3f229c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c00f4e45b8b49996665b",
+    "url": "./static/css/main.d701d976.chunk.css"
+  },
+  {
+    "revision": "c00f4e45b8b49996665b",
+    "url": "./static/js/main.c00f4e45.chunk.js"
+  },
+  {
+    "revision": "5da8b9668f5a1178a029",
+    "url": "./static/css/1.ca8469cc.chunk.css"
+  },
+  {
+    "revision": "5da8b9668f5a1178a029",
+    "url": "./static/js/1.5da8b966.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f80b8a05dd76fdb40e630f1e90b1303d",
+    "url": "./static/media/fa-brands-400.f80b8a05.woff2"
+  },
+  {
+    "revision": "877baf6278a6f1506a07afd23b334f47",
+    "url": "./static/media/fa-brands-400.877baf62.eot"
+  },
+  {
+    "revision": "5e405c16c959bec402cc00609e9806f9",
+    "url": "./static/media/fa-brands-400.5e405c16.woff"
+  },
+  {
+    "revision": "ec813c5b36705e64ba121073b315cb52",
+    "url": "./static/media/fa-regular-400.ec813c5b.eot"
+  },
+  {
+    "revision": "569693c9f5d42d769c7f87b90856219a",
+    "url": "./static/media/fa-regular-400.569693c9.woff2"
+  },
+  {
+    "revision": "418d54a4a9df0e7b3e28dc09e4cce808",
+    "url": "./static/media/fa-brands-400.418d54a4.ttf"
+  },
+  {
+    "revision": "06b3153d3d537a854dc839652018bb87",
+    "url": "./static/media/fa-regular-400.06b3153d.woff"
+  },
+  {
+    "revision": "323b749d60730fa9ab61ff2d70e1cc32",
+    "url": "./static/media/fa-regular-400.323b749d.ttf"
+  },
+  {
+    "revision": "e54c6ae126dd37d48c47dc1aa75cb54e",
+    "url": "./static/media/fa-regular-400.e54c6ae1.svg"
+  },
+  {
+    "revision": "59ea9019c9b9bc4d83ab9783e830735c",
+    "url": "./static/media/fa-solid-900.59ea9019.woff2"
+  },
+  {
+    "revision": "f9103ae53b2dbcb0a14605eebc90a2ce",
+    "url": "./static/media/fa-solid-900.f9103ae5.eot"
+  },
+  {
+    "revision": "b9c86e3abec102a2c9910dfac85c1c17",
+    "url": "./static/media/fa-solid-900.b9c86e3a.ttf"
+  },
+  {
+    "revision": "686e245a4f1a9894d10a576655e932dd",
+    "url": "./static/media/fa-solid-900.686e245a.woff"
+  },
+  {
+    "revision": "1eba168c1b8603ab4dd282f5633d4650",
+    "url": "./static/media/fa-solid-900.1eba168c.svg"
+  },
+  {
+    "revision": "3e30e90a9ee4f94765599bcefd758cb3",
+    "url": "./static/media/fa-brands-400.3e30e90a.svg"
+  },
+  {
+    "revision": "4f777f9daf99bb3a3a5ae319e58c4949",
+    "url": "./index.html"
+  }
+];
